93508ea8bf6b5724a6f73488fcc27d6d2e8d566f
Eric Laukien <ericlaukien@gmail.com>
2019-05-30 12:56:57 -0400

    Tuning

--- source/OptimizerMAB.h
+++ source/OptimizerMAB.h
@@ -38,7 +38,7 @@ namespace swarm {
         int _playTime;
 
         OptimizerMAB()
-        : _timer(0), _alpha(0.001f), _gamma(0.2f), _epsilon(0.6f), _playTime(8)
+        : _timer(0), _alpha(0.1f), _gamma(0.2f), _epsilon(0.6f), _playTime(8)
         {}
 
         void create(ComputeSystem &cs, const std::vector<int> &numParameters, int numArms);
