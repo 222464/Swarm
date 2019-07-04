abaccfb1e1a52294ac6e15d99477646dddf4eb09
Eric Laukien <ericlaukien@gmail.com>
2019-07-04 12:15:54 -0400

    Fixes

--- source/OptimizerDynamic.cpp
+++ source/OptimizerDynamic.cpp
@@ -38,8 +38,8 @@ void OptimizerDynamic::create(ComputeSystem &cs, const std::vector<int> &numPara
     _Ts.resize(numParameters.size());
     _timers.resize(numParameters.size());
 
-    std::uniform_real_distribution<float> CDist(-1.0, 1.0f);
-    std::normal_distribution<float> ADist(0.0f, 1.0f);
+    std::uniform_real_distribution<float> CDist(-0.01f, 0.01f);
+    std::normal_distribution<float> ADist(0.0f, 0.01f);
     std::normal_distribution<float> TDist(_mu, _sigma);
 
     for (int i = 0; i < numParameters.size(); i++) {
