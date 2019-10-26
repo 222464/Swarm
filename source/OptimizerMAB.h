a2e9aaa1756a194e02b33eb34751d146a14a7b93
Eric Laukien <ericlaukien@gmail.com>
2019-10-25 20:39:37 -0400

    Fixes

--- source/OptimizerMAB.h
+++ source/OptimizerMAB.h
@@ -34,7 +34,7 @@ namespace swarm {
         int _playTime;
 
         OptimizerMAB()
-        : _timer(0), _alpha(0.001f), _epsilon(1.0f), _playTime(8)
+        : _timer(0), _alpha(0.01f), _epsilon(1.0f), _playTime(8)
         {}
 
         void create(ComputeSystem &cs, const std::vector<int> &numParameters, int numArms);
